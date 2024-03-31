8a28f55ff275a19541c5848281d3b12980998171
tanercamm <225260023@firat.edu.tr>
2024-03-31 13:21:52 +0300

    Add Paginations

--- BookWeb/Controllers/HomeController.cs
+++ BookWeb/Controllers/HomeController.cs
@@ -5,27 +5,34 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BookWeb.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly BookContext _context;
-        public HomeController(BookContext context)
-        {
-            _context = context;
-        }
+	public class HomeController : Controller
+	{
+		private readonly BookContext _context;
+		public HomeController(BookContext context)
+		{
+			_context = context;
+		}
 
-        public IActionResult Index()
-        {
-            var model = new HomePageViewModel
-            {
-                PopularBooks = _context.Books.ToList()
-            };
-            return View(model);
-        }
+		public int pageSize = 10;
 
-        public IActionResult About()
-        {
-            return View();
-        }
+		public IActionResult Index(int page = 1)
+		{
+			var model = new HomePageViewModel
+			{
+				PopularBooks = _context.Books.Skip((page - 1) * pageSize).Take(pageSize).ToList(),
+				PageInfo = new PageInfo()
+				{
+					ItemsPerPage = pageSize,
+					TotalItems = _context.Books.Count()
+				}
+			};
+			return View(model);
+		}
 
-    }
+		public IActionResult About()
+		{
+			return View();
+		}
+
+	}
 }
